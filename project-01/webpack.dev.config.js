0cbc36662974b50445014b4d4a234f747d80656b
irfan <engr.irfandayan@gmail.com>
2022-09-04 18:15:04 +0500

    Add devlopment and production config

---
+++ project-01/webpack.dev.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, './dist'),
+    // publicPath: 'dist/',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    // contentBase: path.resolve(__dirname, './dist'),
+    // open: 'index.html',
+    // port: 9000,
+    // // writeToDisk: true,
+
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        // use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\. js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['transform-class-properties'],
+          },
+        },
+      },
+      {
+        test: /\.hbs$/,
+        use: ['handlebars-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      // filename: 'style.css',
+      filename: 'styles.[contenthash].css',
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        '**/*',
+        path.join(process.cwd(), 'build/**/*'),
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Hello World',
+      // filename: 'subfolder/custom_filename.html',
+      template: 'src/index.hbs',
+      description: 'Some description',
+      // meta: {
+      //   description: 'Some description',
+      // },
+    }),
+  ],
+};
